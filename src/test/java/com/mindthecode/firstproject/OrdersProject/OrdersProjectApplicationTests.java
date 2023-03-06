67084556d9939d3fe891ce1376318078e7023e65
Marios Mitelos <marios.mitelos@gmail.com>
2023-03-06 20:13:12 +0200

    initial commit

---
+++ src/test/java/com/mindthecode/firstproject/OrdersProject/OrdersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindthecode.firstproject.OrdersProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
